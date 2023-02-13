a2e718f16baf3117b88574f07b056377162957ed
stormytuna <stormytuna@outlook.com>
2023-02-13 16:24:11 +0000

    Document lerp

--- package/lerp.js
+++ package/lerp.js
@@ -1,5 +1,13 @@
 const { clamp } = require("./helpers");
 
+/**
+ * Linear interpolation between the start and end value
+ * @param {number} start Starting value, returned when amount is 0
+ * @param {number} end Ending value, returned when amount is 1
+ * @param {number} amount The lerp percentage
+ * @param {boolean} clamped Whether or not to clamp the amount between 0 and 1. Defaults to true
+ * @returns The lerped value
+ */
 module.exports.lerp = (start, end, amount, clamped = true) => {
   amount = clamped ? clamp(amount, 0, 1) : amount;
   return start + (end - start) * amount;
