8a77c17eb1b1967bdeec128e632bc0fe07aa3286
stormytuna <stormytuna@outlook.com>
2023-02-13 14:13:58 +0000

    Add clamp parameter to easeIn

--- package/helpers.js
+++ package/helpers.js
@@ -1,3 +1,15 @@
 module.exports.lerp = (start, end, amount) => {
   return start + (end - start) * amount;
 };
+
+module.exports.clamp = (value, min, max) => {
+  if (value <= min) {
+    return min;
+  }
+
+  if (value >= max) {
+    return max;
+  }
+
+  return value;
+};
--- package/index.js
+++ package/index.js
@@ -1,4 +1,4 @@
-const { lerp } = require("./helpers");
+const { lerp, clamp } = require("./helpers");
 
 /**
  * Ease in interpolation (smooth start) between the start and end value.
@@ -6,16 +6,11 @@ const { lerp } = require("./helpers");
  * @param {number} end Ending value, returned when amount is 1
  * @param {number} amount The lerp percentage, values < 0 and > 1 might result in weird results when unclamped
  * @param {number} power The exponent of the easing curve, larger values result in more easing
+ * @param {boolean} clamped Whether or not to clamp the amount between 0 and 1. Defaults to true
  * @returns An eased value between start and end
  */
-module.exports.easeIn = (start, end, amount, power) => {
-  if (amount <= 0) {
-    return start;
-  }
-  if (amount >= 1) {
-    return end;
-  }
-
+module.exports.easeIn = (start, end, amount, power, clamped = true) => {
+  amount = clamped ? clamp(amount, 0, 1) : amount;
   const easingAmount = Math.pow(amount, power);
   return lerp(start, end, easingAmount);
 };
