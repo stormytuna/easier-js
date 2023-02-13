04e311436b3b25078db4d51525c5cd68a6224b82
stormytuna <stormytuna@outlook.com>
2023-02-13 14:38:58 +0000

    Add easeOut function and tests

--- package/index.js
+++ package/index.js
@@ -14,3 +14,22 @@ module.exports.easeIn = (start, end, amount, power, clamped = true) => {
   const easingAmount = Math.pow(amount, power);
   return lerp(start, end, easingAmount);
 };
+
+/**
+ * Ease out interpolation (smooth stop) between the start and end value.
+ * @param {number} start Starting value, returned when amount is 0
+ * @param {number} end Ending value, returned when amount is 1
+ * @param {number} amount The lerp percentage, values < 0 and > 1 might result in weird results when unclamped
+ * @param {number} power The exponent of the easing curve, larger values result in more easing
+ * @param {boolean} clamped Whether or not to clamp the amount between 0 and 1. Defaults to true
+ * @returns An eased value between start and end
+ */
+module.exports.easeOut = (start, end, amount, power, clamped = true) => {
+  amount = clamped ? clamp(amount, 0, 1) : amount;
+
+  const flippedAmount = 1 - amount;
+  const flippedEasingAmount = Math.pow(flippedAmount, power);
+  const easingAmount = 1 - flippedEasingAmount;
+
+  return lerp(start, end, easingAmount);
+};
--- tests/__tests__/index.test.js
+++ tests/__tests__/index.test.js
@@ -1,4 +1,4 @@
-const { easeIn } = require("easier-js");
+const { easeIn, easeOut } = require("easier-js");
 
 describe("easeIn", () => {
   it("should return the correct value when amount is between 0 and 1", () => {
@@ -21,5 +21,29 @@ describe("easeIn", () => {
   it("should not clamp amount when clamp is false", () => {
     expect(easeIn(0, 1, -1, 1, false)).toBeLessThan(0);
     expect(easeIn(0, 1, 2, 1, false)).toBeGreaterThan(1);
+  });
+});
+
+describe("easeOut", () => {
+  it("should return the correct value when amount is between 0 and 1", () => {
+    expect(easeOut(0, 1, 0.5, 2)).toBeCloseTo(0.75, 2);
+    expect(easeOut(0, 1, 0.5, 3)).toBeCloseTo(0.875, 3);
+    expect(easeOut(0, 1, 0.5, 4)).toBeCloseTo(0.9375, 4);
+    expect(easeOut(0, 1, 0.2, 2)).toBeCloseTo(0.36, 2);
+    expect(easeOut(0, 1, 0.2, 3)).toBeCloseTo(0.488, 3);
+    expect(easeOut(0, 1, 0.2, 4)).toBeCloseTo(0.59, 2);
+    expect(easeOut(0, 1, 0.7, 2)).toBeCloseTo(0.9, 1);
+    expect(easeOut(0, 1, 0.7, 3)).toBeCloseTo(0.973, 3);
+    expect(easeOut(0, 1, 0.7, 4)).toBeCloseTo(0.9919, 4);
+  });
+
+  it("should clamp amount when clamp is true", () => {
+    expect(easeOut(0, 1, -5, 1, true)).toBe(0);
+    expect(easeOut(0, 1, 50, 1, true)).toBe(1);
+  });
+
+  it("should not clamp amount when clamp is false", () => {
+    expect(easeOut(0, 1, -1, 1, false)).toBeLessThan(0);
+    expect(easeOut(0, 1, 2, 1, false)).toBeGreaterThan(1);
   });
 });
